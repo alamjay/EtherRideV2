9ea81f1b6e507b9583dc585138458690908bd8ba
jayalam <jayalam.97@gmail.com>
2019-03-07 19:09:40 +0000

    Added additional test cases and removed temp files

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-const MyStringStore = artifacts.require("MyStringStore");
 const Vehicleshare = artifacts.require("Vehicleshare");
 
 module.exports = function(deployer) {
-  deployer.deploy(MyStringStore);
   deployer.deploy(Vehicleshare);
 };
--- test/Vehicleshare.js
+++ test/Vehicleshare.js
@@ -1,55 +1,43 @@
-// const Vehicleshare = artifacts.require("./Vehicleshare.sol");
+const Vehicleshare = artifacts.require("./Vehicleshare");
 
-
-// contract("Vehicleshare", function(acocunts){
-//     it("Should register a vehicle", function() {
-//         // return Vehicleshare.deployed().then(function(instance){
-//         //     make = "BMW";
-//         //     model = "3 Series";
-//         //     price = 1;
-//         //     location = "London"
-//         //     return instance.setVehicle(make, model, price, location, {from: accounts[0]});
-//         // }).then(function(){
-//         //     return console.log(instance.getVehicle("0xf52699a790e8130170a2c8f9080cb42e11a1a75c"));
-//         // })
-
-//         return Vehicleshare.numVehicles().then(function(res){
-//             assert.equal(res, 0);
-//         });
-//     });
-// });
-
-const Vehicleshare = artifacts.require("Vehicleshare");
-
-contract('Vehicleshare', (accounts) => {
+contract("Vehicleshare", accounts => {
+  it("should register the vehicle", async () => {
+    const vehicleshare = await Vehicleshare.deployed();
 
     // Owner should be able to register the vehicle onto the platform
-    it('should register the vehicle', function(){
-        Vehicleshare.deployed()
-          .then((instance) => {
-              instance.setVehicle("BMW", "M3", 1, "London", {from: accounts[0]});
-            return instance.vehicles.call(accounts[0]);
-          }).then((getVeh) => {
-            assert.equal(getVeh.make, "BMW", "They don't match");
-            assert.equal(getVeh.model, "M3", "They don't match");
-            assert.equal(getVeh.price, 1, "They don't match");
-            assert.equal(getVeh.location, "London", "They don't match");
-          })
-      });
+    await vehicleshare.setVehicle("BMW", "M3", 1, "London", {from: accounts[0]});
 
-      it('should reserve a vehicle', function(){
-          Vehicleshare.deployed()
-          .then((instance => {
-            instance.setVehicle("BMW", "M3", 1, "London", {from: accounts[0]});
-              instance.setHire(accounts[0], "07032018T11:30", "07032018T12:30", {from: accounts[1]});
-              return instance.rentals.call(1);
-          }))
-          .then((getRental) => {
-              assert.equal(getRental.driver, accounts[1]);
-              assert.equal(getRental.vehicle, accounts[0]);
-              assert.equal(getRental.start_date_time, "07032018T11:30");
-              assert.equal(getRental.end_date_time, "07032018T12:30");
-              assert.equal(getRental.location, "London");
-          })
-      })
+    // get vehicle from public vehices variable
+    const getVehicle = await vehicleshare.vehicles.call(accounts[0]);
+
+    assert.equal(getVehicle.make, "BMW", "The vehicle make don't match");
+    assert.equal(getVehicle.model, "M3", "The vehicle model don't match");
+    assert.equal(getVehicle.price, 1, "The price don't match");
+    assert.equal(getVehicle.location, "London", "The location don't match");
+
+  });
+
+  it("should reserve a vehicle", async () => {
+    const vehicleshare = await Vehicleshare.deployed();
+    await vehicleshare.setHire(accounts[0], "07032018T11:30", "07032018T12:30", {from: accounts[1], value: 1000000000000000000});
+    const getHire = await vehicleshare.rentals.call(1);
+    
+    assert.equal(getHire.driver, accounts[1], "The accounts don't match");
+    assert.equal(getHire.vehicle, accounts[0], "The accounts don't match");
+    assert.equal(getHire.start_date_time, "07032018T11:30", "The times don't match");
+    assert.equal(getHire.end_date_time, "07032018T12:30", "The times don't match");
+    assert.equal(getHire.location, "London", "The location don't match");
+  });
+
+  it("should notify the owner when driver reserves vehicle", async () => {
+    const vehicleshare = await Vehicleshare.deployed();
+    const getOwnerRequest = await vehicleshare.acceptDriver.call(accounts[1], 1, {from: accounts[0]});
+    assert.equal(getOwnerRequest, true, "No results from acceptDriver method");
+  });
+
+  it("should transfer funds from contract to owner", async () => {
+    const vehicleshare = await Vehicleshare.deployed();
+    const getConfirmRequest = await vehicleshare.confirmRequest(accounts[0], {from: accounts[1]});
+    assert.equal(getConfirmRequest.receipt.gasUsed, 30966, "The gasUsed don't match");
+  });
 });
